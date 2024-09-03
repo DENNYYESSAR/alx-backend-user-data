fec920f79554101ab05edc523a836d35b30442e3
DENNYYESSAR <dennisokeri5@gmail.com>
2024-09-03 19:25:26 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """GET /api/v1/unauthorized
     Return:
       - Unauthorized error.
@@ -35,8 +35,8 @@ def unauthorized() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
     """GET /api/v1/forbidden
     Return:
       - Forbidden error.
